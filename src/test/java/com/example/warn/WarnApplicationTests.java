dcc3e3c28480fe289dd91143b244d597a0da9bb1
lsl <1215288388@qq.com>
2021-07-29 21:40:22 +0800

    warn

---
+++ src/test/java/com/example/warn/WarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
