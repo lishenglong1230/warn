d2d1322ce1f38e9ddf1f0fcf04d716fc269bbd53
lwy123-wq <l2158223895@qq.com>
2021-08-13 20:46:25 +0800

    warn

--- src/main/java/com/example/warn/entity/Message.java
+++ src/main/java/com/example/warn/entity/Message.java
@@ -1,5 +1,17 @@
 package com.example.warn.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
 public class Message {
-    
+    private int id;
+    private String name;
+    private String carNumber;
+    private String railway;
+    private String type;
+    private String date;
 }
